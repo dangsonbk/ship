c62688556e8cbc69a796f0fb984ca8906aefa06b
dangsonbk <dangson302@gmail.com>
2022-12-23 17:44:21 +0700

    update calculation rate

--- laytime/app/models.py
+++ laytime/app/models.py
@@ -10,6 +10,31 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Document(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    path = models.CharField(max_length=1000, blank=False)
     description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='documents/')
+    document = models.FileField(upload_to='/media/')
+    parsed = models.BooleanField(default=False)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class Laysheet(models.Model):
+    document = models.OneToOneField(Document, on_delete=models.CASCADE)
+    vehicle = models.CharField(max_length=1000, blank=False)
+    vehicle_mother = models.CharField(max_length=1000, blank=False)
+    discharge_port = models.CharField(max_length=1000, blank=False)
+    NOR_tendered = models.CharField(max_length=1000, blank=False)
+    NOR_accepted = models.CharField(max_length=1000, blank=False)
+    commenced_discharging = models.CharField(max_length=1000, blank=False)
+    completed_discharging = models.CharField(max_length=1000, blank=False)
+    cargo_quantity = models.CharField(max_length=50, blank=False)
+    discharge_rate = models.CharField(max_length=50, blank=False)
+    demurrage = models.CharField(max_length=50, blank=False)
+    despatch = models.CharField(max_length=50, blank=False)
+    laytime_allowed = models.CharField(max_length=50, blank=False)
+
+class Laytime(models.Model):
+    pass
+
+class Charge(models.Model):
+    contract = models.CharField(max_length=255, blank=False)
+    calculation = models.CharField(max_length=1000)
---
+++ xlsx/python_reader_test.py
@@ -0,0 +1,35 @@
+from openpyxl import load_workbook
+import os
+
+def load_excel(path):
+    wb = load_workbook(path, data_only=True)
+    for ws in wb:
+        title = ws["A1"].value
+        if "LAYTIME CALCULATION" in title:
+            print("valid worksheet", ws.title)
+        vehicle = ws["D2"].value
+        vehicle_mother = ws["D3"].value
+        discharge_port = ws["B5"].value
+        NOR_tendered = ws["G5"].value
+        NOR_accepted = ws["G7"].value
+
+        commenced_discharging = discharge_port = ws["G9"].value
+        completed_discharging = discharge_port = ws["G11"].value
+        cargo_quantity = discharge_port = ws["B9"].value
+        discharge_rate = discharge_port = ws["B11"].value
+        demurrage = discharge_port = ws["B13"].value
+        despatch = discharge_port = ws["B14"].value
+        laytime_allowed = discharge_port = ws["B16"].value
+
+        print(vehicle, vehicle_mother, discharge_port, NOR_tendered, NOR_accepted)
+        print(commenced_discharging, completed_discharging, cargo_quantity, discharge_rate, demurrage, despatch, laytime_allowed)
+
+        for row in ws.iter_rows(min_row=20, max_row=24):
+            print("---")
+            for cell in row:
+                print(cell.value)
+
+for root, dirs, files in os.walk("./sample", topdown=False):
+    for name in files:
+        if "DUBAIKNIGHT" in name:
+            load_excel(os.path.join(root, name))
